5e94e74be82a866b2d246b89dc51664f1a149dc0
andre ferrer <andrerferrer@hotmail.com>
2021-02-25 18:57:07 -0300

    implement working init-qr-reader js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,9 +26,8 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { initQrReader } from '../plugins/init_qr_reader';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  initQrReader();
 });
